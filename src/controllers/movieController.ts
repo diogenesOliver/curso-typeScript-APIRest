de72d82bb32d9c2b57f03ac9475f0b0af512c2fb
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-09-09 19:45:24 -0300

    Creating a new controller

---
+++ src/controllers/movieController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+
+import { MovieModel } from '../models/Movie'
+import Logger from '../../config/logger'
+
+export async function createMovie(req: Request, res: Response){
+
+    return res.status(200).send('Filme criado com sucesso')
+
+}
