373235b9761f235af5bd497f96a21e28bd91c49f
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-09-11 13:03:38 -0300

    Importing middleware validator in route

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import {Router, Request, Response} from 'express'
 import { createMovie } from './controllers/movieController'
 
+import { validate } from './middleware/handleValidation'
+
 const router = Router()
 
 export default router.get('/teste', (req: Request, res: Response) => {
 
     res.status(200).send('API Funcionando até então')
 
-}).post('/movie', createMovie)
+}).post('/movie', validate, createMovie)
